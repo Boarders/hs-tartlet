bfd8e23ecd1dec41d79f40a6e46f3ac1fe3dd973
Boarders <callan.mcgill@gmail.com>
2020-06-18 17:13:17 -0400

    Add eval function

--- src/Core/Eval.hs
+++ src/Core/Eval.hs
@@ -6,7 +6,33 @@ import Core.Context
 
 
 eval :: Env -> Expr -> Value
-eval = undefined
+eval env (Var v) = evalVar env v
+eval env (Pi a dom dep) = VPi (eval env dom) (Closure env a dep)
+eval env (Lam v body) = VLam (Closure env v body)
+eval env (App fn arg) = doApply (eval env fn) (eval env arg)
+eval env (Sigma a carT cdrT) = VSigma (eval env carT) (Closure env a cdrT)
+eval env (Cons f s) = VPair (eval env f) (eval env s)
+eval env (Car p) = doCar (eval env p)
+eval env (Cdr p) = doCdr (eval env p)
+eval env Nat = VNat
+eval env Zero = VZero
+eval env (Add1 n) = VAdd1 (eval env n)
+eval env (IndNat tgt mot base step)
+  = doIndNatStep (eval env tgt) (eval env mot) (eval env base) (eval env step)
+eval env (Equal ty from to)
+  = VEqual (eval env ty) (eval env from) (eval env to)
+eval env Same = VSame
+eval env (Replace eq mot base) =
+  doReplace (eval env eq) (eval env mot) (eval env base)
+eval env Trivial = VTrivial
+eval env Sole = VSole
+eval env Absurd = VAbsurd
+eval env (IndAbsurd false ty) = doIndAbsurd (eval env false) (eval env ty)
+eval env Atom = VAtom
+eval env (Tick chrs) = VTick chrs
+eval env U = VU
+eval env (The _ e) = eval env e
+
 
 evalClosure :: Closure -> Value -> Value
 evalClosure (Closure env var body) val = eval (extendEnv env var val) body
@@ -36,9 +62,15 @@ doApply (VNeutral (VPi domT depT) neu) arg =
     VNeutral subDepT (NApp neu (Normal domT arg))
 doApply fun arg = tyCheckError "doApply" [fun, arg]
 
+
+doIndAbsurd :: Value -> Value -> Value
+doIndAbsurd (VNeutral VAbsurd neu) mot =
+  VNeutral mot (NIndAbsurd neu (Normal VU mot))
+doIndAbsurd v mot = tyCheckError "doIndAbsurd" [v, mot]
+
 doReplace :: Value -> Value -> Value -> Value
 doReplace VSame _ base = base
-doReplace (VNeutral (VEq ty from to) neu) mot base =
+doReplace (VNeutral (VEqual ty from to) neu) mot base =
   let
     transTgt = doApply mot to
     motT     = VPi ty (Closure emptyEnv newVar U)
@@ -109,7 +141,7 @@ readBackTyped ctx (VSigma carT cdrT) pair =
 readBackTyped ctx VTrivial _ = Sole
 readBackTyped ctx VAbsurd (VNeutral VAbsurd neu) =
   The Absurd (readBackNeutral ctx neu)
-readBackTyped ctx (VEq _ _ _) VSame = Same
+readBackTyped ctx (VEqual _ _ _) VSame = Same
 readBackTyped ctx VAtom (VTick chars) = Tick chars
 readBackTyped ctx VU VNat = Nat
 readBackTyped ctx VU VTrivial = Trivial
@@ -117,7 +149,7 @@ readBackTyped ctx VU VAbsurd  = Absurd
 readBackTyped ctx VU VAtom    = Atom
 -- This needs to change when universes are added
 readBackTyped ctx VU VU = U
-readBackTyped ctx VU (VEq t from to) =
+readBackTyped ctx VU (VEqual t from to) =
   Equal
     (readBackTyped ctx VU t)
     (readBackTyped ctx t from)
